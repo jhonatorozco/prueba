e57eaed66ebebecd8a7178970675cf6835c77e84
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-05-24 18:44:37 -0500

    Inserto impresion en la cclase Prueba.java

--- src/holaGit/Prueba.java
+++ src/holaGit/Prueba.java
@@ -3,8 +3,9 @@ package holaGit;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("Este proyecto lo voy a sbir a git <3");
+		System.out.println("Este proyecto lo voy a sbir a git");
 		System.out.println("Este proyecto lo voy a subir* a git <3");
+		System.out.println("Otra linea");
 	}
 
 }
