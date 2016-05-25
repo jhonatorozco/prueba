6338c2a343b705b689211ceb51838812b4842d0c
yoiner.gomez <yoiner.gomez22@gmail.com>
2016-05-24 19:17:07 -0500

    Merge

--- src/holaGit/Prueba.java
+++ src/holaGit/Prueba.java
@@ -3,14 +3,10 @@ package holaGit;
 public class Prueba {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Este proyecto lo voy a sbir a git <3");
+
+		System.out.println("Este proyecto lo voy a subir a git <3");
 		System.out.println("<3 <3 <3");
-=======
-		System.out.println("Este proyecto lo voy a sbir a git");
-		System.out.println("Este proyecto lo voy a subir* a git <3");
 		System.out.println("Otra linea");
->>>>>>> e57eaed66ebebecd8a7178970675cf6835c77e84
 	}
 
 }
